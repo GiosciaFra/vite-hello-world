8e57c898f4c04789944134792aee3e100c8b95b6
giosciafra <francescogioscia8@gmail.com>
2024-02-28 13:13:59 +0100

    Esercizio finito (no bonus)

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+    name: 'AppTitle',
+}
+
+</script>
+
+<template>
+
+    <h1>La mia prima app con Vite!</h1>
+
+</template>
+
+<style>
+
+h1 {
+    color: aqua;
+    font-size:large;
+    font-weight: bolder;
+}
+
+</style>
